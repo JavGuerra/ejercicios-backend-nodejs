e3c01304b7de3e7c566f565aa27f281cd0fcca39
JavGuerra <javguerra@badared.com>
2022-07-05 16:53:12 +0200

    refactorización

--- 01-rutas/src/js/index.js
+++ 01-rutas/src/js/index.js
@@ -21,49 +21,46 @@ const fs   = require('fs').promises;
 
 const escuchaPeticion = (req, res) => {
 
-    if (req.method === 'GET') {
-        if (req.url !== '/favicon.ico') { // evita doble petición
+    if (req.method === 'GET' && req.url !== '/favicon.ico') { // evita doble petición
 
-            res.setHeader("Content-Type", "text/html");
+        res.setHeader("Content-Type", "text/html");
 
-            const rutas = ['/', '/archivo', '/contacto', '/donde', '/que', '/quien'];
-            let ruta = './src';
+        const rutas = ['/', '/archivo', '/contacto', '/donde', '/que', '/quien'];
+        let ruta = './src';
 
-            if (!rutas.includes(req.url)) {
-                escribeAviso('No encontrado', 404);
-                return;
-            } else if (req.url == '/') {
-                ruta += '/index.html';
-            } else {
-                ruta += `${req.url}.html`;
-            }
+        if (!rutas.includes(req.url)) {
+            escribeAviso('No encontrado', 404);
+            return;
+        } else if (req.url == '/') {
+            ruta += '/index.html';
+        } else {
+            ruta += `${req.url}.html`;
+        }
 
-            if (ruta == './src/archivo.html') {
-                fs.writeFile('./archivo.txt', 'Texto de prueba.')
-                    .then(escribeAviso('Archivo guardado'))
-                    .catch(err => {
-                        escribeAviso(err.code, 500);
-                        return;
-                    });
-            } else {
-                fs.readFile(ruta, 'utf8')
-                    .then(contenido => {
-                        res.statusCode;
-                        res.end(contenido);
-                    })
-                    .catch(err => {
-                        escribeAviso(err.code, 500);
-                        return;
-                    });
-            }
+        if (ruta == './src/archivo.html') {
+            fs.writeFile('./archivo.txt', 'Texto de prueba.')
+                .then(escribeAviso('Archivo guardado'))
+                .catch(err => {
+                    escribeAviso(err.code, 500);
+                    return;
+                });
+        } else {
+            fs.readFile(ruta, 'utf8')
+                .then(contenido => {
+                    res.statusCode;
+                    res.end(contenido);
+                })
+                .catch(err => {
+                    escribeAviso(err.code, 500);
+                    return;
+                });
+        }
 
-            function escribeAviso(err, num = 200) {
-                let msg = num < 300 ? 'OK' : 'Error';
-                console.log(`${msg} ${num}.`);
-                res.statusCode = num;
-                res.end(`<h1>${msg} ${num}: ${err}</h1>`);
-            }
-
+        function escribeAviso(err, num = 200) {
+            let msg = num < 300 ? 'OK' : 'Error';
+            console.log(`${msg} ${num}.`);
+            res.statusCode = num;
+            res.end(`<h1>${msg} ${num}: ${err}</h1>`);
         }
     }
 };
