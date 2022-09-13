b1a7e11b6cb9efb33bc5fe817ce493d64e69557c
JavGuerra <javguerra@badared.com>
2022-09-13 12:21:10 +0200

    postgres

--- 08-postgresql/src/controllers/searchControllers.js
+++ 08-postgresql/src/controllers/searchControllers.js
@@ -1,21 +1,13 @@
-const { response, r } = require('../modules/utils');
+const { response } = require('../modules/utils');
 const searchServices = require('../services/searchServices');
 
 const getProducts = async (req, res) => {
   const { modelo, color, precio, marca } = req.query;
   const code = (modelo || color || precio || marca) ? 0 : 2;
   let products = '';
-
   if (!code) {
-    let params = '';
-    params += modelo ? r(params) + `name LIKE '%${modelo.toUpperCase()}%'` : '';
-    params += color  ? r(params) + `color LIKE '%${color.toLowerCase()}%'` : '';
-    params += precio ? r(params) + `price < ${precio}` : '';
-    params += marca  ? r(params) + `manufacter_cif='${marca}'` : '';
-
-    products = await searchServices.getProducts(params);
+    products = await searchServices.getProducts(modelo, color, precio, marca);
   }
-
   res.json(response(code, products));
 };
 
--- 08-postgresql/src/services/searchServices.js
+++ 08-postgresql/src/services/searchServices.js
@@ -1,6 +1,11 @@
-const { getDbList } = require('../modules/utils');
+const { getDbList, r } = require('../modules/utils');
 
-const getProducts = (params) => {
+const getProducts = (modelo, color, precio, marca) => {
+    let params = '';
+    params += modelo ? r(params) + `name LIKE '%${modelo.toUpperCase()}%'` : '';
+    params += color  ? r(params) + `color LIKE '%${color.toLowerCase()}%'` : '';
+    params += precio ? r(params) + `price < ${precio}` : '';
+    params += marca  ? r(params) + `manufacter_cif='${marca}'` : '';
     const request = 'SELECT * FROM products' + params + ';';
     console.log(request);
     return getDbList(request);
