89b7cad47654ce8652ea136bc9803aabf8fa0276
JavGuerra <javguerra@badared.com>
2022-09-13 15:27:32 +0200

    mysql

--- 09-mysql/src/modules/connection.js
+++ 09-mysql/src/modules/connection.js
@@ -1,12 +1,12 @@
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const mysql = require('mysql');
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const conexion = mysql.createConnection({
     host : process.env.DB_HOST,
     database : process.env.DB_DATABASE,
     user : process.env.DB_USERNAME,
     password : process.env.DB_PASSWORD,
-    port : process.env.DB_PORT,
+    port : process.env.DB_PORT
 });
 
 conexion.connect(function(err) {
