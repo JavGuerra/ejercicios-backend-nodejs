18f9e61b9a97f21cdc5f354aca24ae58ba523a27
JavGuerra <javguerra@badared.com>
2022-10-16 21:49:03 +0200

    backend

--- 10-backend-mongodb/src/services/searchServices.js
+++ 10-backend-mongodb/src/services/searchServices.js
@@ -1,14 +1,14 @@
 const productModel = require('../schemas/Product-schema');
 
 const getFilteredProducts = async (modelo, color, precio, marca) => {
-    const query = {};
-    if (modelo) query.name  = { $regex: `.*${modelo}.*` };
-    if (color ) query.color = { $regex: `.*${color}.*` };
-    if (precio) query.price = { $lt: precio };
-    if (marca ) query.manufacter = { $regex: `.*${marca}.*` };
+    const filter = {};
+    if (modelo) filter.name  = { $regex: `.*${modelo}.*` };
+    if (color ) filter.color = { $regex: `.*${color}.*` };
+    if (precio) filter.price = { $lt: precio };
+    if (marca ) filter.manufacter = { $regex: `.*${marca}.*` };
     
-    return await productModel.find(query).exec();
-    // return await productModel.find(query).populate('manufacter._id').exec();
+    return await productModel.find(filter).exec();
+    // return await productModel.find(filter).populate('manufacter._id').exec();
 }
 
 module.exports = { getFilteredProducts };
