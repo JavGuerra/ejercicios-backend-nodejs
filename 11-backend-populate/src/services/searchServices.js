8a18a62fc75581271767f3aa6bdfb949500dac53
JavGuerra <javguerra@badared.com>
2022-10-29 14:19:24 +0200

    backend

--- 11-backend-populate/src/services/searchServices.js
+++ 11-backend-populate/src/services/searchServices.js
@@ -6,7 +6,7 @@ const getFilteredProducts = async (modelo, color, precio, marca) => {
     if (modelo) filter.name  = { $regex: `.*${modelo}.*` };
     if (color ) filter.color = { $regex: `.*${color}.*`  };
     if (precio) filter.price = { $lte: precio };
-    if (marca ) filter["manufacturer.name"] = marca;
+    if (marca ) filter["manufacturer.name"] = { $regex: `.*${marca}.*` };;
     
     return await Product.find( filter, {_id: 0} )
         .populate( 'manufacturer.ref', '-_id cif address' ).exec();
