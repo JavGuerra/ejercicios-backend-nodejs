2c40a51f72ca1c5a602867ac1b0819915a8798d4
JavGuerra <javguerra@badared.com>
2022-10-29 10:57:20 +0200

    backend

--- 12-backend-paginate/src/services/searchServices.js
+++ 12-backend-paginate/src/services/searchServices.js
@@ -9,7 +9,7 @@ const getFilteredProducts = async (modelo, color, precio, marca, page, limit) =>
     if (marca ) filter["manufacturer.name"] = marca;
 
     const populate = {path: 'manufacturer.ref', select: '-_id cif address'};
-    
+
     return await Product.paginate(filter, { page, limit, populate });
 
     // return await Product.paginate({}, { page, sort: { ['price']: 1 }, limit });
