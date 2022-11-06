e033c5d0b5406fdd73d857a66d57aa1013dc431a
JavGuerra <javguerra@badared.com>
2022-11-06 10:58:21 +0100

    backend

--- 12-backend-paginate/src/controllers/productControllers.js
+++ 12-backend-paginate/src/controllers/productControllers.js
@@ -1,24 +1,24 @@
 const searchServices = require('../services/productServices');
 
 const getFilteredProducts = async (req, res) => {
-  let { modelo, color, precio, marca, page = 1, limit = 5 } = req.query;
-  if (modelo) modelo = modelo.trim().toUpperCase();
-  if (color ) color  = color.trim().toLowerCase();
-  if (precio) precio = precio.trim();
-  if (marca ) marca  = marca.trim().toUpperCase();
-  page  = page  ? page  : 1;
-  limit = limit ? limit : 5;
+  let { model, color, price, brand, page = 1, limit = 5, sortprice = 1 } = req.query;
+  if (model) model = model.trim().toUpperCase();
+  if (color) color = color.trim().toLowerCase();
+  if (price) price = price.trim();
+  if (brand) brand = brand.trim().toUpperCase();
   if (isNaN(page)  || page  < 1) page  = 1;
   if (isNaN(limit) || limit < 1) limit = 5;
+  if (sortprice !== '-1') sortprice = 1;
 
   const filter = {};
-  if (modelo) filter.name  = { $regex: `.*${modelo}.*` };
-  if (color ) filter.color = { $regex: `.*${color}.*`  };
-  if (precio) filter.price = { $lte: precio };
-  if (marca ) filter["manufacturer.name"] = { $regex: `.*${marca}.*` };
+  if (model) filter.name  = { $regex: `.*${model}.*` };
+  if (color) filter.color = { $regex: `.*${color}.*`  };
+  if (price) filter.price = { $lte: price };
+  if (brand) filter["manufacturer.name"] = { $regex: `.*${brand}.*` };
 
+  const sort = { price: sortprice };
   const populate = { path: 'manufacturer.ref', select: '-_id cif address' };
-  const options  = { page, limit, populate };
+  const options  = { sort, populate, page, limit };
 
   result = await searchServices.getFilteredProducts(filter, options);
   response_code = (result.docs.length) ? 0 : 1;
